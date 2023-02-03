4a352cc30dbd2d0a484f7442edcb69c45e5bc72b
JAI KANT YADAV <123864884+Yjaikant@users.noreply.github.com>
2023-02-04 01:26:42 +0530

    Add files via upload

---
+++ aa/index.js
@@ -0,0 +1,39 @@
+const { toNamespacedPath } = require("path")
+
+// Practice Set Q1 //
+let a = "JaiKant"
+let b = "5"
+console.log(a + b)
+
+// Practice Set Q2 // 
+console.log(typeof (a + b))
+
+// Practice Set Q3 //
+
+const a1={
+  name: "JaiKant",
+    section: 1,
+   isPrincipal: false
+ }
+//a1=45
+
+// Practice Set Q4 //
+
+a1['friend'] = "Aniket"
+a1['lover'] = "Munni"
+a1['hobby'] = "Dance and Gym"
+console.log(a1)
+
+// Practice Set Q5 //
+
+const dict ={
+  Brisk: "Fast",
+  Didactic: "Intended to teach",
+  Impend: "That will happen soon",
+  Juttout: "Anything sticking out",
+  Obviate: "Remove"
+  
+}
+
+console.log (dict)
+
